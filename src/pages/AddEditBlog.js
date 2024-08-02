eaa830e112ee7a7043d6c2ac45f7227934ff276c
sravan-kumar-ta <sravan0095@gmail.com>
2024-08-02 23:50:22 +0530

    image upload to firebase done

--- src/pages/AddEditBlog.js
+++ src/pages/AddEditBlog.js
@@ -7,7 +7,10 @@ import {
   MDBValidation,
   MDBValidationItem,
 } from "mdb-react-ui-kit";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { storage } from "../firebase";
+import { toast } from "react-toastify";
 
 const initalState = {
   title: "",
@@ -20,6 +23,43 @@ const AddEditBlog = () => {
   const [progress, setProgress] = useState(null);
 
   const { title, description } = data;
+
+  useEffect(() => {
+    const updaloadFIle = () => {
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + " % done.");
+          setProgress(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.error("Upload failed:", error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            toast.info("Image uploaded successfully.");
+            setData((prev) => ({ ...prev, imgURL: downloadURL }));
+          });
+        }
+      );
+    };
+    file && updaloadFIle();
+  }, [file]);
 
   const handleChange = (e) => {};
 
@@ -76,7 +116,12 @@ const AddEditBlog = () => {
               />
             </div>
             <div className="col-12">
-              <MDBBtn style={{ width: "50%" }}>Submit</MDBBtn>
+              <MDBBtn
+                style={{ width: "50%" }}
+                disabled={progress !== null && progress < 100}
+              >
+                Submit
+              </MDBBtn>
             </div>
           </MDBValidation>
         </MDBCardBody>
