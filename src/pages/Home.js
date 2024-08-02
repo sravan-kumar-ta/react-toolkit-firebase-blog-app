52f1d9646802f99928681eedcd33f013603fbb9d
sravan-kumar-ta <sravan0095@gmail.com>
2024-08-03 01:27:16 +0530

    fetch all blogs using rtk-query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Spinner from "../components/Spinner";
+import { useFetchBlogsQuery } from "../services/blogsApi";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardText,
+  MDBCardTitle,
+  MDBCol,
+  MDBIcon,
+  MDBRow,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+  const { data, isLoading, isError, error } = useFetchBlogsQuery();
 
-export default Home
+  useEffect(() => {
+    isError && toast.error(error);
+  }, [isError]);
+  
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const handleDelete = () => {};
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1200px",
+        alignContent: "center",
+      }}
+    >
+      <MDBRow className="row-cols-1 row-cols-md-3 g-4">
+        {data?.map((item) => (
+          <MDBCol key={item.id}>
+            <MDBCard className="h-100">
+              <MDBCardImage src={item.imgURL} alt={item.title} position="top" />
+              <MDBCardBody>
+                <MDBCardTitle className="text-start">{item.title}</MDBCardTitle>
+                <MDBCardText className="text-start">
+                  {item.description}
+                </MDBCardText>
+                <div style={{ marginLeft: "5px", float: "right" }}>
+                  <MDBBtn className="mt-1" tag="a" color="none">
+                    <MDBIcon
+                      fas
+                      icon="trash"
+                      style={{ color: "#dd4d39" }}
+                      onClick={() => handleDelete(item.id)}
+                      size="lg"
+                    />
+                  </MDBBtn>
+                  <Link to={`/update/${item.id}`}>
+                    <MDBIcon
+                      fas
+                      icon="edit"
+                      style={{ color: "#55acee", marginLeft: "10px" }}
+                      size="lg"
+                    />
+                  </Link>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        ))}
+      </MDBRow>
+    </div>
+  );
+};
+
+export default Home;
--- src/services/blogsApi.js
+++ src/services/blogsApi.js
@@ -1,5 +1,10 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  serverTimestamp,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 export const blogApi = createApi({
@@ -7,8 +12,21 @@ export const blogApi = createApi({
   baseQuery: fakeBaseQuery(),
   endpoints: (builder) => ({
     fetchBlogs: builder.query({
-      queryFn() {
-        return { data: "ok" };
+      async queryFn() {
+        try {
+          const blogRef = collection(db, "blogs");
+          const querySnapshot = await getDocs(blogRef);
+          let blogs = [];
+          querySnapshot?.forEach((doc) => {
+            blogs.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          });
+          return { data: blogs };
+        } catch (err) {
+          return { error: err };
+        }
       },
     }),
     addBlog: builder.mutation({
